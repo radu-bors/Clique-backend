81599a4d36ef350a0720d621a14700df6ccde142
radu-bors <github.artuproar@8alias.com>
2023-08-14 13:40:14 +0200

    Dockerized API for quick test of auth token functionality

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Depends, HTTPException, Header
+
+app = FastAPI()
+
+def verify_URL_token(token: str = ""):
+    if token != "AreYouThere?":
+        raise HTTPException(status_code=403, detail="Invalid access token")
+    return token
+
+@app.get("/status_URL_token")
+def read_root(token: str = Depends(verify_URL_token)):
+    return {"message": "Yep, I'm functional with URL tokens"}
+
+def verify_header_token(token: str = Header(default=None)):
+    if token != "AreYouThere?":
+        raise HTTPException(status_code=403, detail="Invalid access token")
+    return token
+
+@app.get("/status_header_token")
+def read_root(token: str = Depends(verify_header_token)):
+    return {"message": "Yep, I'm functional with header tokens"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
