e3644c99688232585a28b77f4a15801bdb2c0a89
radu-bors <github.artuproar@8alias.com>
2023-08-17 20:03:31 +0200

    fixed wrong import

--- classes.py
+++ classes.py
@@ -1,19 +1,20 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 import uuid
+from datetime import datetime
 
 class User(BaseModel):
-    user_id: uuid.UUID
+    user_id: uuid.UUID = uuid.uuid4()
     first_name: str
     last_name: str
     middle_name: Optional[str] = None
-    username: Optional[str] = None
+    username: str
     email: EmailStr
     birthdate: str
     gender: str
-    location: str
+    location: List[float]
     profile_photo_url: Optional[str] = None
     description: Optional[str] = None
-    last_online: Optional[str] = None
-    is_online: bool = False
+    last_online: datetime = datetime.now()
     social_media_links: Optional[dict] = None
+
